964257cc443a3a67405dd2dcfad8cc648e5a8a96
unknown <cschlesser@oneall.com>
2012-02-27 20:47:23 +0100

    Revert "Issue #1455564 by tinker: Code now respects Drupal standards and"
    This reverts commit 22356b11e6c852bfda089faa649dc5cc147cdfdf.

--- oneall_social_login.providers.php
+++ oneall_social_login.providers.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * @file
- * Contains the list of available providers for the OneAll Social Login module
- */
-
-/**
  * @package   	OneAll Social Login Providers
  * @copyright 	Copyright 2012 http://www.oneall.com - All rights reserved.
  * @license   	GNU/GPL 2 or later
@@ -29,34 +24,51 @@
  */
 
 /**
+ * @file
+ * Contains the list of available providers for the OneAll Social Login module
+ */
+
+/**
  * Available providers
  */
 $oneall_social_login_available_providers = array(
   'facebook' => array(
-    'name' => 'Facebook',
-  ), 'twitter' => array(
-    'name' => 'Twitter',
-  ), 'google' => array(
-    'name' => 'Google',
-  ), 'linkedin' => array(
-    'name' => 'LinkedIn',
-  ), 'yahoo' => array(
-    'name' => 'Yahoo',
-  ), 'openid' => array(
-    'name' => 'OpenID',
-  ), 'wordpress' => array(
-    'name' => 'Wordpress.com',
-  ), 'hyves' => array(
-    'name' => 'Hyves',
-  ), 'paypal' => array(
-    'name' => 'PayPal',
-  ), 'livejournal' => array(
-    'name' => 'LiveJournal',
-  ), 'vkontakte' => array(
-    'name' => 'VKontakte',
-  ), 'stackexchange' => array(
-    'name' => 'StackExchange',
-  ), 'steam' => array(
-    'name' => 'Steam Community',
+    'name' => 'Facebook'
+  ),
+  'twitter' => array(
+    'name' => 'Twitter'
+  ),
+  'google' => array(
+    'name' => 'Google'
+  ),
+  'linkedin' => array(
+    'name' => 'LinkedIn'
+  ),
+  'yahoo' => array(
+    'name' => 'Yahoo'
+  ),
+  'openid' => array(
+    'name' => 'OpenID'
+  ),
+  'wordpress' => array(
+    'name' => 'Wordpress.com'
+  ),
+  'hyves' => array(
+    'name' => 'Hyves'
+  ),
+  'paypal' => array(
+    'name' => 'PayPal'
+  ),
+  'livejournal' => array(
+    'name' => 'LiveJournal'
+  ),
+  'vkontakte' => array(
+    'name' => 'VKontakte'
+  ),
+  'stackexchange' => array(
+    'name' => 'StackExchange'
+  ),
+  'steam' => array(
+    'name' => 'Steam Community'
   ),
 );
