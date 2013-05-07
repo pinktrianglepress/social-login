309549c8d02d03a3dbc73b218849cfad2fcbea15
Bill Wu <bill.wu@wulei.ca>
2013-05-07 14:36:21 -0400

    this patch is customized to meet Daily Xtra's needs for User creation.

--- theme/block_provider_container.tpl.php
+++ theme/block_provider_container.tpl.php
@@ -4,8 +4,24 @@
  * @file
  * Container in which the social network icons are displayed.
  */
-
+global $base_url;
 ?>
 <div class="oneall_social_login" style="margin:20px 0 10px 0">
- <div id="<?php echo filter_xss($containerid); ?>"></div>
+ <div id="<?php echo filter_xss($containerid); ?>"><img src="<?php print $base_url.'/sites/all/themes/dailyxtra/images/sociallogin.png'; ?>"></div>
+  <div id="top_user_info_bar">
+   <?php
+     if(user_is_logged_in()){
+       global $user;
+       print '| ';
+       print '<span class="user-name">' . l($user->name, 'user') . '</span>';
+       print ' | ';
+       print '<span class="sign-out">' . l('Sign out', 'user/logout') . '</span>';
+     }else{
+       print '| ';
+       print '<span class="register">' . l('Register', 'user/register') . '</span>';
+       print ' | ';
+       print '<span class="sign-in">' . l('Sign in', 'sign-in') . '</span>';
+     }
+   ?>
+ </div>
 </div>
